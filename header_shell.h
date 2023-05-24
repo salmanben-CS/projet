95f8cbe8ea51bd11bde0965a1876f12bd0da7ff6
salman ben omar <salmanbenomar250@gmail.com>
2023-05-24 03:59:24 +0100

    Update header_shell.h

--- header_shell.h
+++ header_shell.h
@@ -5,7 +5,12 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+#include <sys/types.h>
 #include <sys/wait.h>
+#include <sys/stat.h>
+#include <limits.h>
+#include <fcntl.h>
+#include <errno.h>
 
 void displayPrompt(void);
 void executeCommand(char *command);
