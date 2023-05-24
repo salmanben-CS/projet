2c98d2379e7d5caa68263b403430485a73238516
Unicorn-7-Ninja <122676935+Unicorn-7-Ninja@users.noreply.github.com>
2023-05-25 00:32:09 +0100

    Update betty_test.c

--- betty_test.c
+++ betty_test.c
@@ -1,74 +1,17 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <string.h>
-#include <sys/types.h>
-#include <sys/wait.h>
-
-#define BUFFER_SIZE 1024
-
-int main(void)
+#include "header_shell.h"
+/**
+ * _digit - a function that checks for a digit (0 through 9)
+ * @c: digit to be checked
+ * Return: 1 if c is uppercase 0 otherwise
+ */
+int _digit(int c)
 {
-    char *line;
-    char *args[64];
-    int status;
-
-    while (1)
-    {
-        printf("$ ");
-
-        line = (char *)malloc(sizeof(char) * BUFFER_SIZE);
-        if (!line)
+	if (c >= '0' && c <= '9')
 	{
-            perror("malloc error");
-            exit(EXIT_FAILURE);
-        }
-
-        if (!fgets(line, BUFFER_SIZE, stdin)) 
+		return (1);
+	}
+	else
 	{
-            free(line);
-            break;
-        }
-
-        // Remove the newline character from the end of the line
-        line[strcspn(line, "\n")] = '\0';
-
-        // Tokenize the line into arguments
-        char *token;
-        token = strtok(line, " ");
-        int i = 0;
-        while (token != NULL) 
-	{
-            args[i] = token;
-            token = strtok(NULL, " ");
-            i++;
-        }
-        args[i] = NULL;
-
-        // Fork a child process
-        pid_t pid = fork();
-        if (pid < 0) {
-            perror("fork error");
-            exit(EXIT_FAILURE);
-        }
-	    else if (pid == 0) 
-	{
-            // Child process
-            if (execvp(args[0], args) == -1) 
-	    {
-                perror("execvp error");
-                exit(EXIT_FAILURE);
-            }
-            exit(EXIT_SUCCESS);
-        } 
-	    else 
-	    {
-            // Parent process
-            waitpid(pid, &status, 0);
-        }
-
-        free(line);
-    }
-
-    return 0;
+		return (0);
+	}
 }
