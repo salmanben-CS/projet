0d0fb4e3c379c9644c260a58e9e363cb2b1f1bfa
Unicorn-7-Ninja <122676935+Unicorn-7-Ninja@users.noreply.github.com>
2023-05-27 15:32:52 +0100

    Update bulltin_fun.c

--- bulltin_fun.c
+++ bulltin_fun.c
@@ -1,15 +1,16 @@
 #include "shell.h"
+
 /**
- * exit_bul - Exit Statue Shell Characteres
+ * exit_bul - Exit Shell with specified status
  * @cmd: Parsed Command
  * @input: User Input
- * @argv:Program Name
- * @c:Excute Count
- * Return: Void (Exit Statue)
+ * @argv: Program Name
+ * @c: Execute Count
+ * Return: Void (Exit Status)
  */
-void  exit_bul(char **cmd, char *input, char **argv, int c)
+void exit_bul(char **cmd, char *input, char **argv, int c)
 {
-	int statue, i = 0;
+	int status, i = 0;
 
 	if (cmd[1] == NULL)
 	{
@@ -17,6 +18,7 @@ void  exit_bul(char **cmd, char *input, char **argv, int c)
 		free(cmd);
 		exit(EXIT_SUCCESS);
 	}
+
 	while (cmd[1][i])
 	{
 		if (_isalpha(cmd[1][i++]) != 0)
@@ -26,41 +28,40 @@ void  exit_bul(char **cmd, char *input, char **argv, int c)
 		}
 		else
 		{
-			statue = _atoi(cmd[1]);
+			status = _atoi(cmd[1]);
 			free(input);
 			free(cmd);
-			exit(statue);
+			exit(status);
 		}
 	}
 }
 
-
 /**
- * change_directory - Change Dir
+ * change_directory - Change current working directory
  * @cmd: Parsed Command
- * @er: Statue Last Command Excuted
- * Return: 0 Succes 1 Failed (For Old Pwd Always 0 Case No Old PWD)
+ * @er: Status of Last Executed Command
+ * Return: 0 for success, 1 for failure
  */
-int change_dir(char **cmd, __attribute__((unused))int er)
+int change_directory(char **cmd, __attribute__((unused)) int er)
 {
-	int value = -1;
+	int result = -1;
 	char cwd[PATH_MAX];
 
 	if (cmd[1] == NULL)
-		value = chdir(getenv("HOME"));
+		result = chdir(getenv("HOME"));
 	else if (_strcmp(cmd[1], "-") == 0)
 	{
-		value = chdir(getenv("OLDPWD"));
+		result = chdir(getenv("OLDPWD"));
 	}
 	else
-		value = chdir(cmd[1]);
+		result = chdir(cmd[1]);
 
-	if (value == -1)
+	if (result == -1)
 	{
 		perror("hsh");
 		return (-1);
 	}
-	else if (value != -1)
+	else if (result != -1)
 	{
 		getcwd(cwd, sizeof(cwd));
 		setenv("OLDPWD", getenv("PWD"), 1);
@@ -68,15 +69,16 @@ int change_dir(char **cmd, __attribute__((unused))int er)
 	}
 	return (0);
 }
+
 /**
- * dis_env - Display Enviroment Variable with shell
- * @cmd:Parsed Command
- * @er:Statue of Last command Excuted
- * Return:Always 0
+ * dis_env - Display Environment Variables
+ * @cmd: Parsed Command
+ * @er: Status of Last Executed Command
+ * Return: Always 0
  */
 int dis_env(__attribute__((unused)) char **cmd, __attribute__((unused)) int er)
 {
-size_t i;
+	size_t i;
 	int len;
 
 	for (i = 0; environ[i] != NULL; i++)
@@ -87,13 +89,14 @@ size_t i;
 	}
 	return (0);
 }
+
 /**
- * display_help - Displaying Help For Builtin with shell
- * @cmd:Parsed Command
- * @er: Statue Of Last Command Excuted
- * Return: 0 Succes -1 Fail
+ * display_help - Display help for built-in commands
+ * @cmd: Parsed Command
+ * @er: Status of Last Executed Command
+ * Return: 0 for success, -1 for failure
  */
-int display_help(char **cmd, __attribute__((unused))int er)
+int display_help(char **cmd, __attribute__((unused)) int er)
 {
 	int fd, fw, rd = 1;
 	char c;
@@ -104,6 +107,7 @@ int display_help(char **cmd, __attribute__((unused))int er)
 		perror("Error");
 		return (0);
 	}
+
 	while (rd > 0)
 	{
 		rd = read(fd, &c, 1);
@@ -113,19 +117,21 @@ int display_help(char **cmd, __attribute__((unused))int er)
 			return (-1);
 		}
 	}
+
 	_putchar('\n');
 	return (0);
 }
+
 /**
- * echo_bul - Excute Echo Cases with shell
- * @st:Statue Of Last Command Excuted
+ * echo_bul - Execute echo command
  * @cmd: Parsed Command
- * Return: Always 0 Or Excute Normal Echo
+ * @st: Status of Last Executed Command
+ * Return: 0 for success or execute normal echo
  */
 int echo_bul(char **cmd, int st)
 {
 	char *path;
-	unsigned int  pid = getppid();
+	unsigned int pid = getppid();
 
 	if (_strncmp(cmd[1], "$?", 2) == 0)
 	{
@@ -136,7 +142,6 @@ int echo_bul(char **cmd, int st)
 	{
 		print_number(pid);
 		PRINTER("\n");
-
 	}
 	else if (_strncmp(cmd[1], "$PATH", 5) == 0)
 	{
@@ -144,7 +149,6 @@ int echo_bul(char **cmd, int st)
 		PRINTER(path);
 		PRINTER("\n");
 		free(path);
-
 	}
 	else
 		return (print_echo(cmd));
