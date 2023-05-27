9cd0efd111c987768753070ee00f03ba26681142
Unicorn-7-Ninja <122676935+Unicorn-7-Ninja@users.noreply.github.com>
2023-05-27 02:42:11 +0100

    Create more_bul_fun.c

--- more_bul_fun.c
+++ more_bul_fun.c
@@ -1,7 +1,7 @@
 #include "shell.h"
 
 /**
- * history_dis - Display History Of User Input Simple Shell
+ * history_dis - Display History
  * @c:Parsed Command
  * @s:Statue Of Last Excute
  * Return: 0 Succes -1 Fail
@@ -36,7 +36,7 @@ int history_dis(__attribute__((unused))char **c, __attribute__((unused))int s)
 	return (0);
 }
 /**
- * print_echo - Excute Normal Echo
+ * print_echo - Execute Normal Echo 
  * @cmd: Parsed Command
  * Return: 0 Succes -1 Fail
  */
