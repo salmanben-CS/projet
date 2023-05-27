1d1fb4b456af94865362fea8f6253239cb242a2c
Unicorn-7-Ninja <122676935+Unicorn-7-Ninja@users.noreply.github.com>
2023-05-27 02:48:49 +0100

    Create printer_fun.c

--- printer_fun.c
+++ printer_fun.c
@@ -1,6 +1,6 @@
 #include "shell.h"
 /**
- * print_number -Print Unsigned Int Putchar
+ * print_number -Print Unsigned Int Putchar and run
  * @n: Unisigned Integer
  * Return: Void
  */
@@ -14,7 +14,7 @@ void print_number(unsigned int n)
 	_putchar(x % 10 + '0');
 }
 /**
- * print_number_in -Print Number Putchar
+ * print_number_in -Print Number Putchar and run
  * @n:Integer
  * Return: void
  */
