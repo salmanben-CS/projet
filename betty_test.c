d3a81b1c60a600aec9b5fc7cf8d553071866bc89
Unicorn-7-Ninja <122676935+Unicorn-7-Ninja@users.noreply.github.com>
2023-05-27 10:41:46 +0100

    Update betty_test.c

---
+++ betty_test.c
@@ -0,0 +1,17 @@
+#include "header_shell.h"
+/**
+ * _digit - a function that checks for a digit (0 through 9)
+ * @c: digit to be checked
+ * Return: 1 if c is uppercase 0 otherwise
+ */
+int _digit(int c)
+{
+	if (c >= '0' && c <= '9')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
